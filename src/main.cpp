5b5833e7a07a411d121e107465d02ab169ac66b7
dudaoqi12 <1523402094@qq.com>
2023-05-04 23:04:12 -0700

    最终版线程池

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include "ThreadPool.hpp"
 #include <unistd.h> // sleep(1);
-#include <vector>
 #include <thread>
-#include <string>
-#include <chrono>
 #include <future>
 #include <array>
 using namespace std;
 
-//测试用例
-int main()
-{
+
+//测试用例1
+void test1(){
+	// 构建线程池  最少存在2个线程 最多有10个
+	ThreadPool* pool = new ThreadPool(2,10);
+	// 启动线程池
+	pool->run();
+	//返回期物
+	auto result = pool->addTask([](int a,int b)->int{sleep(5);return a+b;},88,88);
+	cout<<"主线程干自己的事情\n";
+	sleep(2);
+	//看看期货是否执行完毕 如果没完毕等待执行完毕 否则程序继续进行
+	result.wait();
+	//得到期货 88+88
+	cout<<result.get()<<"\n";
+}
+
+
+//测试用例2
+void test2(){
 	// 构建线程池
 	ThreadPool* pool = new ThreadPool(2,10);
 	// 启动线程池
@@ -46,5 +60,12 @@ int main()
 		cout<<"result"<<i<<": "<<result[i].get()<<"\n";
 	}
 
+}
+
+
+//测试用例
+int main()
+{
+	test1();
 	return 0;
 }
